374ed33d7d0dae9c3756e11b42a1f1d446711f94
Nenova221 <kalina.nenova@paisii.info>
2024-08-23 15:59:36 +0300

    First commit

---
+++ BitOperation1/BitOperation1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitOperation
+{
+    public class BitOperation
+    {
+        static void Main(string[] args)
+        {
+            int v = 35, p = 6, i = 1, mask = i << p;
+            bool isOne = (v & mask) != 0;
+            if ((v & mask) != 0)
+            {
+                Console.WriteLine("Third bit is 1");
+            }
+            else
+            {
+                Console.WriteLine("Third bit is 0");
+            }
+        }
+    }
+}
